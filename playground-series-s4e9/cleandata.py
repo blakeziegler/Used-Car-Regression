4401dfc0bac6cc9e3b54a452cbd159d2fbd4953b
Blake Ziegler <zieglerblake2@gmail.com>
2024-09-17 13:41:13 -0400

    reclean

--- playground-series-s4e9/cleandata.py
+++ playground-series-s4e9/cleandata.py
@@ -1,10 +1,9 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
 
 
 # Load the data
-df = pd.read_csv('train.csv')
+df = pd.read_csv('test.csv')
 
 '''
 --------------------
@@ -126,9 +125,9 @@ df = df.drop(columns=['clean_title'])
 '''
 
 # Fill missing values with grouped means
-df['hp'] = df.groupby(['fuel_encoded', 'brand_encoded', 'transmission_encoded', pd.cut(df['year_scaled'], bins=10)], observed=False)['hp'].transform(lambda x: x.fillna(x.mean()))
-df['tank_size'] = df.groupby(['fuel_encoded', 'brand_encoded','transmission_encoded', pd.cut(df['year_scaled'], bins=10)], observed=False)['tank_size'].transform(lambda x: x.fillna(x.mean()))
-df['cyl'] = df.groupby(['fuel_encoded', 'brand_encoded', 'transmission_encoded', pd.cut(df['year_scaled'], bins=10)], observed=False)['cyl'].transform(lambda x: x.fillna(x.mean()))
+df['hp'] = df.groupby(['fuel_encoded', pd.cut(df['year_scaled'], bins=10)], observed=False)['hp'].transform(lambda x: x.fillna(x.mean()))
+df['tank_size'] = df.groupby(['fuel_encoded', pd.cut(df['year_scaled'], bins=10), pd.cut(df['hp'], bins=10)], observed=False)['tank_size'].transform(lambda x: x.fillna(x.mean()))
+df['cyl'] = df.groupby([pd.cut(df['hp'], bins=10)], observed=False)['cyl'].transform(lambda x: x.fillna(x.mean()))
 
 # Fill any remaining NaN values with overall column mean
 df['hp'].fillna(df['hp'].mean(), inplace=True)
@@ -153,4 +152,4 @@ df = df.drop(columns=(['cyl']))
 # Save the cleaned and encoded DataFrame to a new CSV file
 print(df.head())
 print(df.info())
-df.to_csv('clean_train.csv', index=False)
+df.to_csv('clean_test.csv', index=False)
