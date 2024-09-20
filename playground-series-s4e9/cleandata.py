a3b2ab7c4d42183f17bb62ae553d02f16d7d1c54
blakeziegler <zieglerblake2@gmail.com>
2024-09-19 22:03:23 -0400

    model tune

--- playground-series-s4e9/cleandata.py
+++ playground-series-s4e9/cleandata.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+import numpy as np
 scaler = StandardScaler()
 # Load the data
 df = pd.read_csv('test.csv')
@@ -17,7 +18,6 @@ extracted = df['engine'].str.extract(r'(\d+)\s?Cylinder|V(\d+)|Straight (\d+)')
 df['cyl'] = extracted.apply(lambda x: x.dropna(
 ).iloc[0] if not x.dropna().empty else None, axis=1)
 
-df = df.drop(columns=['engine'])
 
 luxury_brands = ['Mercedes-Benz', 'BMW', 'Audi', 'Porsche', 'Land',
                  'Lexus', 'Jaguar', 'Bentley', 'Maserati', 'Lamborghini',
@@ -25,7 +25,33 @@ luxury_brands = ['Mercedes-Benz', 'BMW', 'Audi', 'Porsche', 'Land',
 df['Is_Luxury_Brand'] = df['brand'].apply(
     lambda x: 1 if x in luxury_brands else 0)
 
+df['transmission_type'] = df['transmission'].apply(lambda x: 
+                                                   'manual' if 'm/t' in x or 'manual' in x or  'mt' in x else 
+                                                   'automatic' if 'a/t' in x or 'automatic' in x or  'at' in x else 
+                                                   'CVT' if 'CVT' in x else 
+                                                   'Other')
 
+df = df.drop(columns=['transmission'])
+
+def extract_fuel_type(engine_info):
+    if pd.isna(engine_info):
+        return np.nan
+    if 'Gasoline' in engine_info:
+        return 'Gasoline'
+    elif 'Hybrid' in engine_info:
+        return 'Hybrid'
+    elif 'Flex Fuel' in engine_info or 'E85' in engine_info:
+        return 'Flex Fuel'
+    elif 'Diesel' in engine_info:
+        return 'Diesel'
+    elif 'Electric' in engine_info:
+        return 'Electric'
+    else:
+        return np.nan
+
+df['extracted_fuel_type'] = df['engine'].apply(extract_fuel_type)
+df['fuel_type'].fillna(df['extracted_fuel_type'], inplace=True)
+df.drop(columns=['extracted_fuel_type'], inplace=True)
 '''
 -----------------
 	CLEANING
@@ -48,31 +74,10 @@ df['fuel_type'] = df['fuel_type'].fillna('Unknown')
 ----------------------------
 '''
 
-# BRAND
-brand_mapping = {brand: idx for idx, brand in enumerate(df['brand'].unique())}
 
-df['brand_encoded'] = df['brand'].map(brand_mapping)
-df = df.drop(columns=['brand'])
-
-# MODEL
-model_counts = df['model'].value_counts()
-
-threshold = 25
-
-model_mapping = model_counts[model_counts > threshold].index
-
-model_encoding = {model: idx for idx,
-                  model in enumerate(model_mapping, start=1)}
-model_encoding['Other'] = len(model_mapping) + 1  # Encoding for rare models
-
-df['model_encoded'] = df['model'].apply(
-    lambda x: model_encoding.get(x, model_encoding['Other']))
-df = df.drop(columns=['model'])
-
-
-df['hp'] = df.groupby(['brand_encoded', pd.cut(df['model_year'], bins=5)], observed=False)['hp'].transform(lambda x: x.fillna(x.mean()))
-df['tank_size'] = df.groupby(['brand_encoded', pd.cut(df['model_year'], bins=5)], observed=False)['tank_size'].transform(lambda x: x.fillna(x.mean()))
-df['cyl'] = df.groupby(['brand_encoded', pd.cut(df['model_year'], bins=5)], observed=False)['cyl'].transform(lambda x: x.fillna(x.mean()))
+df['hp'] = df.groupby(['brand', pd.cut(df['model_year'], bins=5)], observed=False)['hp'].transform(lambda x: x.fillna(x.mean()))
+df['tank_size'] = df.groupby(['brand', pd.cut(df['model_year'], bins=5)], observed=False)['tank_size'].transform(lambda x: x.fillna(x.mean()))
+df['cyl'] = df.groupby(['brand', pd.cut(df['model_year'], bins=5)], observed=False)['cyl'].transform(lambda x: x.fillna(x.mean()))
 
 # HP, TANK, CYL
 df['hp'].fillna(df['hp'].mean(), inplace=True)
@@ -86,81 +91,22 @@ df['cyl'].fillna(df['cyl'].mean(), inplace=True)
 df['car_age'] = 2024 - df['model_year']
 df = df.drop(columns=['model_year'])
 
-
-# MILEAGE
-
-# FUEL TYPE
-fuel_mapping = {fuel_type: idx for idx,
-                fuel_type in enumerate(df['fuel_type'].unique())}
-
-df['fuel_encoded'] = df['fuel_type'].map(fuel_mapping)
-df = df.drop(columns=['fuel_type'])
-
-# TRANSMISSION
-transmission_counts = df['transmission'].value_counts()
-
-threshold = 25
-
-transmission_mapping = transmission_counts[transmission_counts >
-                                           threshold].index
-
-transmission_encoding = {transmission: idx for idx,
-                         transmission in enumerate(transmission_mapping, start=1)}
-transmission_encoding['Other'] = len(
-    transmission_mapping) + 1  # Encoding for rare transmissions
-
-df['transmission_encoded'] = df['transmission'].apply(
-    lambda x: transmission_encoding.get(x, transmission_encoding['Other']))
-
-df = df.drop(columns=['transmission'])
-
-# EXT COLOR
-ext_counts = df['ext_col'].value_counts()
-
-threshold = 25
-
-ext_mapping = ext_counts[ext_counts > threshold].index
-
-ext_encoding = {ext: idx for idx,
-                         ext in enumerate(ext_mapping, start=1)}
-ext_encoding['Other'] = len(
-    ext_mapping) + 1  # Encoding for rare transmissions
-
-df['ext_encoded'] = df['ext_col'].apply(
-    lambda x: ext_encoding.get(x, ext_encoding['Other']))
-
-df = df.drop(columns=['ext_col'])
-
-
-# INT COLOR
-int_counts = df['int_col'].value_counts()
-
-threshold = 25
-
-int_mapping = int_counts[int_counts > threshold].index
-
-int_encoding = {int1: idx for idx,
-                         int1 in enumerate(int_mapping, start=1)}
-int_encoding['Other'] = len(
-    int_mapping) + 1  # Encoding for rare transmissions
-
-df['int_encoded'] = df['int_col'].apply(
-    lambda x: int_encoding.get(x, int_encoding['Other']))
-
-df = df.drop(columns=['int_col'])
-
-# ACCIDENT
-df['accident_none_reported'] = (df['accident'] == 'None reported').astype(int)
-df['accident_damage_reported'] = (
-    df['accident'] == 'At least 1 accident or damage reported').astype(int)
-
-df = df.drop(columns=['accident'])
-df = df.drop(columns=['accident_none_reported'])
-
-# CLEAN TITLE
 df['clean_title_yes'] = (df['clean_title'] == 'Yes').astype(int)
 
 df = df.drop(columns=['clean_title'])
+
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
+df['brand'] = encoder.fit_transform(df['brand'])
+df['int_col'] = encoder.fit_transform(df['int_col'])
+df['ext_col'] = encoder.fit_transform(df['ext_col'])
+df['transmission_type'] = encoder.fit_transform(df['transmission_type'])
+df['fuel_type'] = encoder.fit_transform(df['fuel_type'])
+df['model'] = encoder.fit_transform(df['model'])
+df['accident'] = encoder.fit_transform(df['accident'])
+
+
+df = df.drop(columns=['engine'])
 
 
 '''
